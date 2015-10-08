b10a5e8dc76e01ba4db1b4684b11b9d1eb8afa50
madasawethen <tzipora.kaplan@gmail.com>
2015-10-07 20:09:17 -0400

    jquery updates
    made some additions to the jquery and internal nav/s on resume page

--- js/project.js
+++ js/project.js
@@ -1,15 +1,42 @@
 $( document ).ready(function() {
 
-$("section h4").click(function() { 
-	
-	if(!$(this).next().hasClass("open")) {
-		$(".expandable.open").slideUp(400).removeClass("open");
-		// $(this).next().slideToggle(300);
+	$("section h4").click(function() { 
+		
+		if(!$(this).next().hasClass("open")) {
+			$(".expandable.open").slideUp(400).removeClass("open");
 
-		$(this).next().addClass("open");
-		$(".open").slideDown(400);
-	}
+			$(this).next().addClass("open");
+			$(".open").slideDown(400);
+		}
 
+		else {
+			$(".expandable.open").slideUp(400).removeClass("open");
+		}
+
+	});
+
+	$(".expandable nav a").click(function() {
+		$(".expandable nav a.current").removeClass("current");
+		$(".expandable div.current").removeClass("current");
+
+		$(this).addClass("current");
+		var mti_nav = $(this).index();
+		$(".expandable div").eq(mti_nav).addClass("current");
+
+	}); 
+
+	$(".expandable div a.next").click(function() {
+		$(".expandable nav a.current").next().trigger("click");
+	});
+
+	$(".expandable div a.previous").click(function() {
+		$(".expandable nav a.current").prev().trigger("click");
+	});
+
+	$(".expandable div a.next-section").click(function() {
+		$(".expandable.open").parent().next().find("h4").trigger("click");
+	});
 
 });
-});
+
+// add a different class to the divs, or wrap all the divs inside of another div
