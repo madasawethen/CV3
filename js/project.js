723f0ba31c865208312bb2d3ec5ba639b3ad10db
madasawethen <tzipora.kaplan@gmail.com>
2016-03-27 15:45:47 -0400

    js changes for drunk drawer

--- js/project.js
+++ js/project.js
@@ -1,21 +1,36 @@
 $( document ).ready(function() {
 
 	$("section h4").click(function() { 
-		
-		if(!$(this).next().hasClass("open")) {
+		var theme2 = false;
+		if ($(this).parent().hasClass("theme2")) {
+			theme2 = true;
+		}
+		if (!$(this).next().hasClass("open")) {
 			$(".expandable.open").slideUp(400).removeClass("open");
-
+			resetSlideshow();
 			$(this).next().addClass("open");
-			$(".open").slideDown(400);
+			$(".open").slideDown(400, function() {
+				if (theme2) {
+					animateSpans();
+				}
+			});
+			if (theme2) {
+				$(this).parent().removeClass("leo");
+			}
 			drunkHeight();
 
-			if($(this).parent().attr("id") == "mti") {
+			if ($(this).parent().attr("id") == "mti") {
 			$(this).next().find("nav a").eq(0).trigger("click");
 			}
 		}
 
 		else {
-			$(".expandable.open").slideUp(400).removeClass("open");
+			$(".expandable.open").slideUp(400,function(){
+					if (theme2) {
+				$(this).parent().addClass("leo");
+			}
+			}).removeClass("open");
+		
 		}
 
 	});
@@ -65,25 +80,50 @@ $( document ).ready(function() {
 		  e.preventDefault();
 		 var wrapperwidth = $("#wrapper").width();
 		 drunkHeight();
-		  $("span").hide();
+		 	$(".active").removeClass("active").next().addClass("active");
 		    $("#slides").animate({left: "-="+wrapperwidth}, 500, function(){
-		    $("span").each(function(){
-		      $(this).fadeIn($(this).data("speed"));
-		    });
+		    animateSpans();
 		  });
+		  checkArrows(); 
 		});
 
 		$("#prev").click(function(e){
 		  e.preventDefault();
 		  var wrapperwidth = $("#wrapper").width();
 		  drunkHeight();
-		  $("span").hide();
+		  	$(".active").removeClass("active").prev().addClass("active");
 		    $("#slides").animate({left: "+="+wrapperwidth}, 500, function(){
-		    $("span").each(function(){
-		      $(this).fadeIn($(this).data("speed"));
-		    });
+		    animateSpans();
 		  });
+		  checkArrows(); 
 		});
+
+		checkArrows();
+
+		function animateSpans() {
+			$(".slide span").hide();
+			$(".slide span").removeClass();
+			setTimeout(function() {
+				$(".slide.active span").each(function(){
+				$(this).addClass("animated").show().addClass($(this).data("animation"));
+			});
+			}, 1000);
+		}
+
+		function checkArrows() {
+			 if ($(".active").hasClass("fourths")) {
+		    	$("#next").hide();
+		    }
+		     else {
+		    	$("#next").show();
+		    }
+		    if ($(".active").hasClass("halves")) {
+		    	$("#prev").hide();
+		    }
+		    else {
+		    	$("#prev").show();
+		    }
+		}
 
 			function drunkHeight() {
 			  var wrapperWidth = $("#wrapper").width();
@@ -94,11 +134,19 @@ $( document ).ready(function() {
   				$(".slide img").height(newHeight);
 			}
 
+			function resetSlideshow() {
+				$("#slides").stop().animate({left: 0}, 500);
+			  	drunkHeight();
+			  	$("#slides .slide").removeClass("active");
+			  	$("#slides .slide").eq(0).addClass("active");
+			  	checkArrows();
+			  	animateSpans();
+			}
+
 			drunkHeight();
 
 			$(window).resize(function(){
-				$("#slides").stop().animate({left: 0}, 500);
-			  drunkHeight();
+				resetSlideshow();
 			});
 
 
